ac1b7880b69673185daf367420e54c14b2fafbe9
Haikal <fabregasfikri58@gmail.com>
2024-06-12 22:38:29 +0700

    Profile, Login, Register, and Forgot Password Ready to Use

---
+++ app/src/main/java/com/haikal/athena/ui/login/LoginActivity.kt
@@ -0,0 +1,34 @@
+package com.haikal.athena.ui.login
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.haikal.athena.MainActivity
+import com.haikal.athena.databinding.ActivityLoginBinding
+import com.haikal.athena.ui.forgotPassword.ForgotPasswordActivity
+import com.haikal.athena.ui.register.RegisterActivity
+
+class LoginActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityLoginBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        binding.btnLogin.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+            Toast.makeText(this, "Login Success", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.tvForgotPassword.setOnClickListener {
+            startActivity(Intent(this, ForgotPasswordActivity::class.java))
+            Toast.makeText(this, "Reset Password", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.tvLoginToRegister.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+            Toast.makeText(this, "Register an Account", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
--- app/src/main/java/com/haikal/athena/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/haikal/athena/ui/profile/ProfileFragment.kt
@@ -1,13 +1,17 @@
 package com.haikal.athena.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.haikal.athena.OCRActivity
 import com.haikal.athena.databinding.FragmentProfileBinding
+import com.haikal.athena.ui.login.LoginActivity
 
 class ProfileFragment : Fragment() {
 
@@ -28,9 +32,10 @@ class ProfileFragment : Fragment() {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textProfile
-        profileViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        binding.btnLogout.setOnClickListener {
+            startActivity(Intent(requireContext(), LoginActivity::class.java))
+            Toast.makeText(requireContext(), "Logout Success", Toast.LENGTH_SHORT).show()
+            requireActivity().finish()
         }
         return root
     }
